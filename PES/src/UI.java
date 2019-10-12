1f44d3f1c6b75ab4d4a99d54fce167dfd0d69bf6
Yushan Tu <ytuu0003@student.monash.edu>
2019-10-12 13:47:22 +1100

    new test system

---
+++ PES/src/UI.java
@@ -0,0 +1,11 @@
+public class UI {
+    PrimeEvent controller = new PrimeEvent();
+
+    public static void main(String[] args) {
+        UI boundary = new UI();
+        boundary.login();
+    }
+    public void login(){
+        System.out.println("Login menu");
+    }
+}
