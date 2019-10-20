91e8ee7c25554ee37c48ca1e58fae09e82ff7a23
Xinyue Huang <xhua0040@student.monash.edu>
2019-10-20 20:07:21 +1100

    The latest version(1.1)

--- PES/src/Owner.java
+++
@@ -1,5 +0,0 @@
-public class Owner extends User {
-    public Owner(String newUserId, String newPwd, String newFirstName, String newLastName, String newAddress, String newPhone){
-        super(newUserId, newPwd, newFirstName, newLastName, newAddress, newPhone);
-    }
-}
